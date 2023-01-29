0f90e714d6a25c29c669a56b61cdd75a69d0ab5d
James Geddes <j@jamesgeddes.pro>
2023-01-29 21:13:47 +0000

    rename vars to variables to prevent overshadow

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from os import getenv
 from datetime import datetime
 from aws import s3_upload_file, s3_download_file
-import vars
+import variables
 
 load_dotenv()
 client_id = getenv("SPOTIFY_CLIENT_ID")
@@ -13,26 +13,28 @@ client_secret = getenv("SPOTIFY_CLIENT_SECRET")
 def main():
     # 1. Try to get data from S3 bucket - if nothing there then that's ok
     s3_download_file(
-        bucket_name=vars.bucket_name,
-        file_key=vars.file_key,
-        file_name=vars.file_key,
+        bucket_name=variables.bucket_name,
+        file_key=variables.file_key,
+        file_name=variables.file_key,
     )
 
     # 2. Get popularity score from spotify
     popularity = get_popularity(
-        client_id=client_id, client_secret=client_secret, song_id=vars.song_id
+        client_id=client_id,
+        client_secret=client_secret,
+        song_id=variables.song_id,
     )
     timestamp_popularity = f"{datetime.now()}, {popularity}\n"
 
     # 3. Append popularity to data
-    with open(vars.file_key, "a") as file:
+    with open(variables.file_key, "a") as file:
         file.write(timestamp_popularity)
 
     # 4. Load data back to S3 bucket
     s3_upload_file(
-        file_name=vars.file_key,
-        bucket=vars.bucket_name,
-        object_name=vars.file_key,
+        file_name=variables.file_key,
+        bucket=variables.bucket_name,
+        object_name=variables.file_key,
     )
 
     # 5. Cake
